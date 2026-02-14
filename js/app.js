a31279ea2399ab5b5b497597ec5198218cb40729
Viết Lân Trần <120633030+vietlan2003@users.noreply.github.com>
2026-02-14 22:19:28 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -23,14 +23,14 @@ gifs.forEach(gifSrc => {
 // changes for when NO button is clicked
 no.addEventListener("click", () => {
   if (count == 2) {
-    gif.src = "https://media1.tenor.com/m/z-eg2uhf-b0AAAAd/peach-cat-kiss.gif";// gif credit: https://knowyourmeme.com/photos/2738959-mr-fresh-side-eye-cat
+    gif.src = "../resources/rusure.gif";// gif credit: https://knowyourmeme.com/photos/2738959-mr-fresh-side-eye-cat
     text.innerHTML = "You meant to press YES right?🤨";
     yes.style.height = "65%";
     yes.style.width = "60%";
     no.style.width = "30%";
     count++;
   } else if (count == 3) {
-    gif.src = "resources/3shocked-1.gif";// gif credit: https://tenor.com/view/tkthao219-peach-goma-gif-25008901
+    gif.src = "../resources/3shocked-1.gif";// gif credit: https://tenor.com/view/tkthao219-peach-goma-gif-25008901
     text.innerHTML = "Your hand must have slipped right?🥹";
     yes.style.height = "70%";
     yes.style.width = "70%";
@@ -56,7 +56,7 @@ no.addEventListener("click", () => {
 // changes for when YES button is clicked
 yes.addEventListener("click", () => {
   vid.style.display = "block";
-  gif.src = "../resources/idc.gif";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
+  gif.src = "https://media1.tenor.com/m/z-eg2uhf-b0AAAAd/peach-cat-kiss.gif";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
   text.innerHTML = "Knew it babiii 😘";
   yes.innerHTML = '<a href="https://www.instagram.com/viet.lann__/">Message me</a>';// inside the " " put your social media profile link
   yes.style.height = "90%";
@@ -66,5 +66,3 @@ yes.addEventListener("click", () => {
     vid.style.display = "none";
   }, 9000);
 });
-
-
